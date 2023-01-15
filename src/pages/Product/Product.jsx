93072af278fc95733ba5a3675c4c7fac6f42cae2
yildirimkadir <yildirimkad34@gmail.com>
2023-01-15 16:17:29 +0100

    "updated"

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../../components/navbar/Navbar"
-import { Container} from "./Product.style" 
+import { Container, Wrapper, ImgContainer, Image, InfoContainer,Title, Desc, Price} from "./Product.style" 
 import Announcements from '../../components/Announcements/Announcements'
 import NewsLetter from "../../components/NewsLetter/NewsLetter"
 // import Products from "../../components/Products/Products"
@@ -11,6 +11,16 @@ const Product = () => {
     <Container>
         <Navbar />
         <Announcements />
+        <Wrapper>
+            <ImgContainer>
+              <Image src="" />
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Men: Fashion Watch</Title>
+                <Desc>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora facere dolor, officiis culpa ex deleniti. Nesciunt quaerat ut odit natus.</Desc>
+                <Price>300$</Price>
+            </InfoContainer>
+        </Wrapper>
         <NewsLetter />
         <Footer />
     </Container>
