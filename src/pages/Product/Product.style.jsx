6296c8c112f5a79bda842724a83ed7a70af0e432
yildirimkadir <yildirimkad34@gmail.com>
2023-01-16 23:59:47 +0100

    "updated"

--- src/pages/Product/Product.style.jsx
+++ src/pages/Product/Product.style.jsx
@@ -4,3 +4,18 @@ import styled from 'styled-components';
 export const Container = styled.div`
   
 `;
+export const Wrapper = styled.div`
+  
+`;
+export const ImgContainer = styled.div`
+  
+`;
+export const Wrapper = styled.div`
+  
+`;
+export const Wrapper = styled.div`
+  
+`;
+export const Wrapper = styled.div`
+  
+`;
